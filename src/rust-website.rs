6f9dbf0b3b69730aefa1c8075d20508cd7d49e2e
Robert Schardt <robert.schardt@iohk.io>
2024-02-23 13:10:49 +0100

    Remove unneccary loop and add creation of response http_headers.

--- src/rust-website.rs
+++ src/rust-website.rs
@@ -1,10 +1,7 @@
 use std::net::{TcpListener, TcpStream, Shutdown};
-use std::io::{BufReader, Read, Write};
-use std::fs::File;
-use std::time::Instant;
+use std::io::{Read, Write};
 
-const RECV_BYTES_LENGTH: usize = 1500;
-const SEND_BYTES_LENGTH: usize = 3000;
+const RECV_BYTES_LENGTH: usize = 3000;
 
 fn main() -> std::io::Result<()>
 {
@@ -39,26 +36,16 @@ fn handle_client(tcp_socket: &TcpListener) {
 
 fn handle_request(mut tcp_stream: &TcpStream) -> String {
 
-    let mut full_request: Vec<u8> = Vec::new();
-    let connection_timer = Instant::now();
-
     // Request reading
-    while connection_timer.elapsed().as_secs() < 30 {
-
-	let mut read_buffer = [0; RECV_BYTES_LENGTH];
-	let read_bytes = tcp_stream.read(&mut read_buffer).unwrap();
-	let buf_to_read = &read_buffer[0..read_bytes];
-	println!("bytes read: {read_bytes:?}");
-	println!("client message: {read_buffer:?}");
-
-	full_request.extend_from_slice(buf_to_read);
-	if read_bytes == 0 || read_bytes < RECV_BYTES_LENGTH { break; }
-    }
+    let mut read_buffer = [0; RECV_BYTES_LENGTH];
+    let read_bytes = tcp_stream.read(&mut read_buffer).unwrap();
+    let buf_to_read = &read_buffer[0..read_bytes];
+    //println!("bytes read: {read_bytes:?}");
+    //println!("client message: {read_buffer:?}");
 
     // Header parsing
-    let request_string = String::from_utf8(full_request).unwrap();
+    let request_string = String::from_utf8_lossy(buf_to_read);
     let splitted_headers: Vec<&str> = request_string.split("\r\n").collect();
-
     //println!("This is the request: {request_string:?}");
     //println!("Those are the splitted headers: {splitted_headers:?}");
 
@@ -71,25 +58,23 @@ fn handle_request(mut tcp_stream: &TcpStream) -> String {
 
 fn send_response(mut tcp_stream: &TcpStream, get_header: &String) {
 
-    // TODO return appropriate header
+    // TODO return appropriate headers
     //if !get_header.is_empty() -> send method not supported etc.
+    // 405 Method Not Allowed
+    let http_header = "HTTP/1.1 200 OK";
+    let server_header = "Server: rust-webserver";
+    let accept_ranges_header = "Accept-Ranges: bytes";
+    let content_type_header = "Content-Type: text/html";
+    let allow_header = "Allow: GET";
+    // TODO this will require chrono crate, to handle DateTimes
+    //let date_header = format!("Date: {now:?}");
+    //let last_modified_header = "Last-Modified: Tue, 01 Dec 2009 20:18:22 GMT";
+    let body = "<!DOCTYPE html>\n<html>\n  <head>\n    <title>\n      Hello World\n    </title>\n  </head>\n  <body>\n    <pre>Hello World\n    </pre>\n  </body>\n</html>\n";
 
-    // Method 1 from File
-    let test_header = File::open("src/http-headers/test-header.txt").expect("couldn't open file");
-    let mut buf_reader = BufReader::new(test_header);
-    let mut read_buffer = [0; SEND_BYTES_LENGTH];
-
-    let read_bytes = buf_reader.read(&mut read_buffer).expect("couldn't read file buffer");
-    let buf_to_send = &read_buffer[0..read_bytes];
-
-    let string_content = String::from_utf8_lossy(buf_to_send);
-    println!("buffer created, ready to send: {string_content:?}");
-
-    tcp_stream.write(&buf_to_send).expect("unable to write to tcp_stream");
-
-    // Method 2 directly from memory
-    // let written_Bytes = tcp_stream.write_all(b"HTTP/1.1 200 OK\nDate: Sat, 09 Oct 2010 14:28:02 GMT\nServer: Apache\nLast-Modified: Tue, 01 Dec 2009 20:18:22 GMT\nETag: \"51142bc1-7449-479b075b2891b\"\nAccept-Ranges: bytes\nContent-Type: text/html\n\n<!DOCTYPE html>\n<html>\n  <head>\n    <title>\n      Hello World\n    </title>\n  </head>\n  <body>\n    <pre>Hello World\n    </pre>\n  </body>\n</html>\n");
-    // println!("bytes written: {written_Bytes:?}");
+    let server_response = format!("{http_header}\n{server_header}\n{accept_ranges_header}\n{content_type_header}\n{allow_header}\n\n{body}");
+    //let server_response = format!("{http_header}\n{date_header}\n{server_header}\n{last_modified_header}\n{accept_ranges_header}\n{content_type_header}\n{allow_header}\n\n{body}");
+    let written_bytes = tcp_stream.write_all(server_response.as_bytes());
+    //println!("bytes written: {written_bytes:?}");
 
     tcp_stream.flush().expect("unable to flush tcp_stream");
 }
